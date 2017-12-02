3cfa145b35556566551853fc1f72837ed15bfddc
liqiang <974923609@qq.com>
2017-12-02 22:06:12 +0800

    export change

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ var config = {
   },
   insertJsToNextPage: {
     disable: false,
-    jumpInterval: 3,
+    jumpInterval: 2,
     minTime: new Date(2017, 0, 1),
-    maxTime: new Date(2017, 10, 21),
-    isCrawlExist: true,
+    maxTime: new Date(2017, 11, 1),
+    isCrawlExist: false,
     // if true updateNumAt - publishAt
     crawlExistInterval: 1000*60*60*24*3,
     targetBiz: []
@@ -18,9 +18,9 @@ var config = {
     disable: false,
     jumpInterval: 15,
     // 抓取到minTime 就跳转至下一公众号
-    minTime: new Date(2017, 9, 28),
+    minTime: new Date(2017, 10, 25),
     // 自定义最近多久更新的公众号本次就不用抓取
-    maxUpdatedAt: new Date(2017, 10, 25),
+    maxUpdatedAt: new Date(2017, 11, 1),
     targetBiz: [],
     // 程序开始时间
     beginTime: new Date()
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const path = require('path');
 const app = express();
+const config = require('./config');
+const Category = require('./models/Category');
 
 const data = require('./api/data');
 
@@ -21,6 +23,20 @@ app.use('/wechat-data-api', data);
 app.use('/wechat-data', express.static(path.join(__dirname, './client/build')));
 app.get('/wechat-data/*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+// 接口设置抓取此分类内的账号
+app.post('/spider', async (req, res, next) => {
+  let args = req.body;
+  let categoryId = args.categoryId;
+  if (!categoryId) return next();
+  let category = await Category.findOne({ _id: categoryId });
+  if (!category) return next();
+  let msgBizs = category.msgBizs;
+  res.end('ok');
+  if (!msgBizs.length) return;
+  config.insertJsToNextProfile.targetBiz = msgBizs;
+  config.insertJsToNextPage.targetBiz = msgBizs;
 });
 
 const server = http.createServer(app);
