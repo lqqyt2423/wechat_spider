2d7c53f078a00906cefcbabd9dcee3e74b1476d0
liqiang <974923609@qq.com>
2018-04-07 14:36:21 +0800

    add bom to csv then could open by excel

--- utils/exportData.js
+++ utils/exportData.js
@@ -84,7 +84,8 @@ module.exports = class ExportData {
   async toCsv(minDate, maxDate, options = {}) {
     const json = await this.toJson(minDate, maxDate, options);
     const obj = JSON.parse(json);
-    const csv = json2csv({ data: obj });
+    let csv = json2csv({ data: obj });
+    csv = addBom(csv);
     return csv;
   }
 
@@ -109,7 +110,8 @@ module.exports = class ExportData {
    */
   async toStaCsv(minDate, maxDate) {
     const data = await this.calcStatistic(minDate, maxDate);
-    const csv = json2csv({ data });
+    let csv = json2csv({ data });
+    csv = addBom(csv);
     return csv;
   }
 
@@ -247,3 +249,9 @@ module.exports = class ExportData {
   }
 
 };
+
+function addBom(csv) {
+  const bom = Buffer.from('\uFEFF');
+  const csvBuf = Buffer.from(csv);
+  return Buffer.concat([bom, csvBuf]).toString();
+}
