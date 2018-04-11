6e425040aba189d67748bdfac0a44100deb791ab
liqiang <974923609@qq.com>
2018-04-11 10:26:10 +0800

    修改初始配置

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ const config = {
     PROFILE_LIST_KEY: 'wechat_spider:profile_list'
   },
   // 是否用本地图片替换所有的图片请求 加快网络速度
-  isReplaceImg: false,
+  isReplaceImg: true,
   // 是否替换显示在手机上的微信文章正文内容 加快网路速度
-  isReplacePostBody: false,
+  isReplacePostBody: true,
   insertJsToNextPage: {
     // 是否关闭自动跳转页面
     disable: false,
@@ -23,7 +23,7 @@ const config = {
     jumpInterval: 2,
     // 跳转文章发布时间范围
     minTime: new Date(2018, 0, 1),
-    maxTime: new Date(2018, 2, 1),
+    maxTime: new Date(2018, 3, 1),
     // 已有数据的文章是否再抓取
     isCrawlExist: false,
     // if true updateNumAt - publishAt
@@ -31,7 +31,7 @@ const config = {
     // 抓取公众号biz范围
     targetBiz: [],
     // 是否保存文章内容
-    isSavePostContent: false,
+    isSavePostContent: true,
     // 保存内容的形式: html/text
     saveContentType: 'text',
   },
@@ -41,11 +41,11 @@ const config = {
     // 仅scroll 不跳转
     onlyScroll: true,
     // 跳转时间间隔 s
-    jumpInterval: 10,
+    jumpInterval: 5,
     // 抓取到minTime就跳转至下一公众号
     minTime: new Date(2018, 0, 1),
     // 自定义最近多久更新的公众号本次就不用抓取
-    maxUpdatedAt: new Date(2018, 2, 15),
+    maxUpdatedAt: new Date(2018, 3, 1),
     // 抓取公众号biz范围
     targetBiz: [],
     // 程序开始时间
