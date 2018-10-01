c23c330dd37e7c3833e8eba8d9719a2a05464609
liqiang <974923609@qq.com>
2018-10-01 15:58:57 +0800

    设置eslintrc缩进

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,62 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "globals": {
+    "_": true,
+    "$": true,
+    "WeixinJSBridge": true,
+    "sequelize": true,
+    "Sequelize": true,
+    "models": true,
+    "glue": true,
+    "ovt": true
+  },
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
     },
-    "globals": {
-        "_": true,
-        "$": true,
-        "WeixinJSBridge": true,
-        "sequelize": true,
-        "Sequelize": true,
-        "models": true,
-        "glue": true,
-        "ovt": true
-    },
-    "extends": ["eslint:recommended", "plugin:react/recommended"],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module",
-        "ecmaVersion": 2017
-    },
-    "plugins": [
-        "react"
+    "sourceType": "module",
+    "ecmaVersion": 2017
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "no-unused-vars": [
+      1
     ],
-    "rules": {
-        "no-unused-vars": [
-          1
-        ],
-        "no-console": [
-          0
-        ],
-        "react/prop-types": [
-          0
-        ],
-        "react/no-danger": [
-          1
-        ],
-        "indent": [
-            1,
-            2,
-            { "SwitchCase": 1 }
-        ],
-        "linebreak-style": [
-            2,
-            "unix"
-        ],
-        "quotes": [
-            1,
-            "single"
-        ],
-        "semi": [
-            2,
-            "always"
-        ],
-        "require-yield": [0]
-    }
+    "no-console": [
+      0
+    ],
+    "react/prop-types": [
+      0
+    ],
+    "react/no-danger": [
+      1
+    ],
+    "indent": [
+      1,
+      2,
+      { "SwitchCase": 1 }
+    ],
+    "linebreak-style": [
+      2,
+      "unix"
+    ],
+    "quotes": [
+      1,
+      "single"
+    ],
+    "semi": [
+      2,
+      "always"
+    ],
+    "require-yield": [0]
+  }
 };
