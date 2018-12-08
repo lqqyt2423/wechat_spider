4c9ac3d3e4165c36cbda6797ca8b95c0b5f8fffc
liqiang <974923609@qq.com>
2018-12-08 18:01:38 +0800

    修改 models 分页逻辑

--- models/plugins/paginator.js
+++ models/plugins/paginator.js
@@ -3,76 +3,84 @@
 /**
  * Pagination Plugin
  */
-const util = require('util');
+const _ = require('lodash');
 const Query = require('mongoose').Query;
 
-const defaults = {
-  perPage: 20, // 每页条数
-  page   :  1, // 初始页数
-  offset :  0,  // 偏移数
+const DEFAULTS = {
+  perPage: 10, // 每页条数
+  page: 1, // 初始页数
+  offset: 0,  // 偏移数
   maxPerPage: 100 // 最大单页条数
 };
+const DEFAULT_KEYS = Object.keys(DEFAULTS);
 
 /**
  * paginate
  *
  * @param {Object} options
  */
-Query.prototype.paginate = function(options, callback) {
-  let opts = util._extend({}, defaults);
-  opts = util._extend(opts, options);
+Query.prototype.paginate = async function (options, callback) {
+  let opts = _.assign({}, DEFAULTS, options);
 
   // 转换值为数字
-  Object.keys(defaults).forEach(function(k) {
-    opts[k] = Number(opts[k]);
-  });
+  DEFAULT_KEYS.forEach(k => opts[k] = Number(opts[k]));
+
+  let hasCallback = typeof callback === 'function';
 
   let query = this;
   let model = query.model;
   let conditions = query._conditions;
 
-  return new Promise(function(resolve, reject) {
-    model.count(conditions, function(err, count) {
+  // 如果有外部传递过来的count，无需查数据库
+  let count = opts.count || 0;
 
-      opts.perPage = opts.perPage >= opts.maxPerPage ? opts.maxPerPage : opts.perPage;
+  try {
+    if (!count) count = await model.where(conditions).count();
 
-      let _skip = (opts.page - 1) * opts.perPage;
-      _skip += opts.offset;
+    // 计算每页数
+    opts.perPage = opts.perPage >= opts.maxPerPage ? opts.maxPerPage : opts.perPage;
 
-      query.skip(_skip).limit(opts.perPage).exec(function(err, data) {
-        if (err) {
-          typeof callback === 'function' ? reject(callback(err)) : reject(err);
-          return;
-        }
+    let _skip = (opts.page - 1) * opts.perPage;
+    _skip += opts.offset;
 
-        data = data || [];
+    let data = await query.skip(_skip).limit(opts.perPage);
 
-        let current = parseInt(opts.page, 10) || 1;
+    let current = parseInt(opts.page, 10) || 1;
 
-        let offsetCount = count - opts.offset;
-        offsetCount = offsetCount > 0 ? offsetCount : 0;
+    let offsetCount = count - opts.offset;
+    offsetCount = offsetCount > 0 ? offsetCount : 0;
 
-        let totalPages = Math.ceil(offsetCount / opts.perPage);
+    let totalPages = Math.ceil(offsetCount / opts.perPage);
 
-        let prev = !count || current === 1 ? null : current - 1;
-        let next = !count || current === totalPages ? null : current + 1;
+    let prev = !count || current === 1 ? null : current - 1;
+    let next = !count || current === totalPages ? null : current + 1;
 
-        if (!offsetCount) {
-          prev = next = null;
-        }
+    if (!offsetCount) prev = next = null;
 
-        let pager = {
-          data: data,
-          options: opts,
-          current: current,
-          next: next,
-          prev: prev,
-          totalPages: totalPages,
-          count: count
-        };
+    let result = {
+      data: data || [],
+      options: opts,
+      current: current,
+      next: next,
+      prev: prev,
+      totalPages: totalPages,
+      count: count,
 
-        typeof callback === 'function' ? resolve(callback(err, pager)) : resolve(pager);
-      });
-    });
-  });
+      // 直接返回给前端的 metadata
+      metadata: {
+        count,
+        totalPages,
+        currentPage: current,
+        perPage: opts.perPage,
+      },
+    };
+
+    return hasCallback ? callback(null, result) : result;
+  } catch (e) {
+    if (hasCallback) {
+      callback(e);
+    } else {
+      throw e;
+    }
+  }
 };
